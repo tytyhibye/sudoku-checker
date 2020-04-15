52503b7e7da8fad329fbe52242e7a467a6e87920
Tyler Bates <Fondlytyler@gmail.com>
2020-04-15 09:03:58 -0700

    successfully run second test checking for 9 indecies per row/col.

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -1,8 +1,13 @@
 import { Sudoku } from "../src/sudokuCheck"
 
-describe('Sudoku Grid', () => {
+  test('should correctly determine whether each row contains 9 numbers', () => {
+    let sudoku = new Sudoku([1,2,3,4,5,6,7,8,9], [1,2,4,5,6,7,8,9])
+    expect(sudoku.row.length).toEqual(9);
+    expect(sudoku.col.length).toEqual(9);
+  })
+    
 
-  test('should correctly register that all squares are filled', () => {
+  test('should correctly register that the 9 integers in each column have a sum of 45', () => {
     let colTotal = new Sudoku([1,2,3,4,5,6,7,8,9],[]) // this.col and this.row
     let total = colTotal.checkCol() // or new Sudoku().checkCol();
     expect(colTotal.checkCol()).toEqual(total);
@@ -13,4 +18,4 @@ describe('Sudoku Grid', () => {
   //   expect(spaces.row).toEqual(45);
     
   // })
-})
+ 
